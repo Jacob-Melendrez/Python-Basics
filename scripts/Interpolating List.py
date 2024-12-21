030c468df965bbb2a14d673161396c111d7ac85f
Jacob Melendrez <jacob.n.melendrezW@gmail.com>
2024-12-21 17:11:31 -0500

    Added interpolating list script

---
+++ scripts/Interpolating List.py
@@ -0,0 +1,8 @@
+countries = ['CAN','CHL','IND','AUS']
+for country in countries: 
+    print(f"I'm flying to {country}.")
+
+print ("\n")
+
+for country in countries[:2]:
+    print(f"I've been to {country}.")
